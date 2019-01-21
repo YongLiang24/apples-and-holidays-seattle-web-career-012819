49550d50e79fa8d08e82d69b9ee434e5abdb2f2a
yong liang <yongliang24@gmail.com>
2019-01-21 09:30:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,8 +74,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect do |season, holiday|
-      holiday.collect do |holiday, parts|
-        return holiday if parts.include?("BBQ")
+      holiday.collect do |holiday, item|
+        holiday if item.include?("BBQ")
       end
     end.flatten.compact
 end
